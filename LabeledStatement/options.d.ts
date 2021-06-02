dee437f912a37542b822ec04e379718dddc5312b
Siphosethu Nongwe <60358782+nophora@users.noreply.github.com>
2021-06-02 14:41:36 +0200

    Options

--- LabeledStatement/options.d.ts
+++ LabeledStatement/options.d.ts
@@ -1,10 +1,3 @@
-// Type definitions for mkdirp 0.5
-// Project: https://github.com/substack/node-mkdirp
-// Definitions by: Bart van der Schoor <https://github.com/Bartvds>
-//                 mrmlnc <https://github.com/mrmlnc>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-
-/// <reference types="node" />
 
 import fs = require('fs');
 
